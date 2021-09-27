78160ad4c232dd381edb22fa41129c108ae11115
Yonder Peña <djyonder@gmail.com>
2021-09-27 18:35:18 -0500

    5 y ultimo cap completo script 3...

---
+++ d
@@ -0,0 +1,23 @@
+[33mcommit c8b0179efe37e4de38ba0a9233053433b35229dc[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Yonder Peña <djyonder@gmail.com>
+Date:   Mon Sep 27 16:52:55 2021 -0500
+
+    cuarto cap completo script 2...
+
+[33mcommit a021ef34d5fcb8da65fa37601b806182eb181b4e[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: Yonder Peña <djyonder@gmail.com>
+Date:   Mon Sep 27 14:30:28 2021 -0500
+
+    tercer cap completo script 1...
+
+[33mcommit fc291c2f9818bc8b7395da7f69be7d75992bb966[m
+Author: Yonder Peña <djyonder@gmail.com>
+Date:   Mon Sep 27 13:46:14 2021 -0500
+
+    segundo cap completo css
+
+[33mcommit d2c52d1e9977e5c12a7eb28ee7be1c24088f87f9[m
+Author: Yonder Peña <djyonder@gmail.com>
+Date:   Mon Sep 27 13:03:10 2021 -0500
+
+    primer capitulo html
--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const balance = document.getElementById("balance");
-const money_plus = document.getElementById("money_plus");
-const money_minus = document.getElementById("money_minus");
+const money_plus = document.getElementById("money-plus");
+const money_minus = document.getElementById("money-minus");
 const list = document.getElementById("list");
 const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
-const dummyTransactions = [
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 }//AQUI POSIBLE ERROR 
-];
+// const dummyTransactions = [
+//   { id: 1, text: "Flower", amount: -20 },
+//   { id: 2, text: "Salary", amount: 300 },
+//   { id: 3, text: "Book", amount: -10 },
+//   { id: 4, text: "Camera", amount: 150 }//AQUI POSIBLE ERROR 
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+
+let transactions = 
+  localStorage.getItem("transactions") !== null ? localStorageTransactions: [];
 
 //add transaction
 function addTransaction(e) {
@@ -33,6 +38,8 @@ function addTransaction(e) {
     addTransactionsDOM(transaction);
 
     updateValues();
+
+    updateLocalStorage();
 
     text.value = "";
     amount.value = "";
@@ -59,7 +66,7 @@ function addTransactionsDOM(transaction) {
     transaction.amount
   )}</span> <button class="delete-btn" onclick="removeTransaction(${
     transaction.id
-  }">x</button>
+  })">x</button>
   `;
 
   list.appendChild(item);
@@ -69,15 +76,15 @@ function addTransactionsDOM(transaction) {
 function updateValues() {
   const amounts = transactions.map(transaction => transaction.amount);
 
-  const total = amounts.reduce((acc, item) => (acc+= item), 0).toFixed(2);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
   const income = amounts
     .filter(item => item > 0)
-    .reduce((acc, item) => (acc +=item), 0)
+    .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
   
   const expense = (
-    amounts.filter(item => item < 0).reduce((acc, item) => (acc +=item), 0) *
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
     -1
   ).toFixed(2);
    
@@ -86,11 +93,20 @@ function updateValues() {
   money_minus.innerText = `$${expense}`;
   }
 
-  //remove transactio by id
+  //remove transaction by id
   function removeTransaction(id) {
     transactions = transactions.filter(transaction => transaction.id !== id);
 
+    updateLocalStorage();
+
+    updateValues();
+
     init();
+  }
+
+  //update local storage transactions
+  function updateLocalStorage() {
+    localStorage.setItem("transactions", JSON.stringify(transactions));
   }
 
 //init app
